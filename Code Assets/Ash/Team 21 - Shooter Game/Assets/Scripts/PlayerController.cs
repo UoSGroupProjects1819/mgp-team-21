fc1d70a62e8df601992472d594a222d615700601
Ashh <43639486+Ashhkc@users.noreply.github.com>
2019-02-06 15:15:14 +0000

    Player Movement and Rotation added

---
+++ Code Assets/Ash/Team 21 - Shooter Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float Speed = 5.5f;
+
+       void Start ()
+    {
+      
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Movement();
+        LookAtMouse();
+    }
+
+    void Movement()
+    {
+
+        transform.position = new Vector3(transform.position.x + Input.GetAxisRaw("Horizontal") * Time.deltaTime * Speed, transform.position.y + Input.GetAxisRaw("Vertical") * Time.deltaTime * Speed, transform.position.z);
+    }
+
+    void LookAtMouse()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        Vector2 direction = new Vector2(
+            mousePosition.x - transform.position.x,
+            mousePosition.y - transform.position.y
+        );
+
+        transform.up = direction;
+
+    }
+
+}
