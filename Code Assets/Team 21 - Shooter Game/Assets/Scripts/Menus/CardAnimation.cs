41c7d4f1f0245c1c5bbd62abc79d25a1b1ae325d
Ashhkc <43639486+Ashhkc@users.noreply.github.com>
2019-03-27 15:00:44 +0000

    Main Menu
    Added Card button assets

---
+++ Code Assets/Team 21 - Shooter Game/Assets/Scripts/Menus/CardAnimation.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class CardAnimation : MonoBehaviour
+{
+    Animator Animation;
+
+    private Button card;
+    public Sprite Highlighted;
+    private Sprite Original;
+    
+
+	// Use this for initialization
+	void Start ()
+    {
+        Animation = gameObject.GetComponent<Animator>();
+        card = GetComponent<Button>();
+
+        Original = gameObject.GetComponent<Image>().sprite;
+
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        OnMouseEnter();
+        OnMouseExit();
+	}
+
+    private void OnMouseEnter()
+    {
+        Animation.SetTrigger("Card Hover");
+        card.image.sprite = Highlighted;
+    }
+
+    private void OnMouseExit()
+    {
+        Animation.ResetTrigger("Card Hover");
+        card.image.sprite = Original;
+
+    }
+}
