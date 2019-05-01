a058ef136ae5790258038fc8f23ba41a9550028e
Kiril Kostadinov <S190005@uos.ac.uk>
2019-05-01 16:07:18 +0100

    Update Menus
    Changed the Menus and created links between them

---
+++ Code Assets/Team 21 - Shooter Game/Assets/Scripts/Menus/SwitchMenus.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SwitchMenus : MonoBehaviour
+{
+    public Button BackButton;
+    public Button SettingsButton;
+    public bool MenuIsActive = false;
+
+    public GameObject MainMenuUI;
+    public GameObject SettingsMenuUI;
+
+    private void Start()
+    {
+        Button btn1 = BackButton.GetComponent<Button>();
+        btn1.onClick.AddListener(ToggleMainMenu);
+
+        Button btn2 = BackButton.GetComponent<Button>();
+        btn2.onClick.AddListener(ToggleSettings);
+
+
+    }
+
+    public void Update()
+    {
+        if (MenuIsActive == false)
+        {
+            ToggleSettings();
+        }
+        else
+        {
+            ToggleMainMenu();
+        }
+    }
+
+    void ToggleMainMenu()
+    {
+        SettingsMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        MenuIsActive = true;
+        
+
+        MainMenuUI.SetActive(true);
+    }
+    void ToggleSettings()
+    {
+        MainMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        MenuIsActive = false;
+        
+
+        SettingsMenuUI.SetActive(true);
+    }
+}
